75da3c787f75650edf93fd1f1f15985c85905dd3
danilstenkin <danilstenkn57@gmail.com>
2025-03-18 19:42:21 +0500

    feat: Update menu_iteam model

--- models/menu_iteam.go
+++ models/menu_iteam.go
@@ -1,8 +1,13 @@
 package models
 
 type MenuItem struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
+	ID                   int         `json:"id"`
+	Name                 string      `json:"name"`
+	Description          string      `json:"description"`
+	Price                float64     `json:"price"`
+	Category             []string    `json:"category"` // Используем срез строк
+	Allergens            []string    `json:"allergens"`
+	CustomizationOptions interface{} `json:"customization_options"` // Если тебе нужно работать с JSONB
+	Size                 string      `json:"size"`
+	Metadata             interface{} `json:"metadata"` // Для хранения JSON
 }
