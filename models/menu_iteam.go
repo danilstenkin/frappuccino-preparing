b0465f10b5fdbf74750180c1a6a6fdaf13440c6a
danilstenkin <danilstenkin57@gmail.com>
2025-03-19 19:40:24 +0600

    fix: Update MenuItem model for proper JSON parsing

--- models/menu_iteam.go
+++ models/menu_iteam.go
@@ -1,13 +1,13 @@
 package models
 
 type MenuItem struct {
-	ID                   int         `json:"id"`
-	Name                 string      `json:"name"`
-	Description          string      `json:"description"`
-	Price                float64     `json:"price"`
-	Category             []string    `json:"category"` // Используем срез строк
-	Allergens            []string    `json:"allergens"`
-	CustomizationOptions interface{} `json:"customization_options"` // Если тебе нужно работать с JSONB
-	Size                 string      `json:"size"`
-	Metadata             interface{} `json:"metadata"` // Для хранения JSON
+	ID                   int                    `json:"id"`
+	Name                 string                 `json:"name"`
+	Description          string                 `json:"description"`
+	Price                float64                `json:"price"`
+	Category             []string               `json:"category"`
+	Allergens            []string               `json:"allergens"`
+	CustomizationOptions map[string]interface{} `json:"customization_options"`
+	Size                 string                 `json:"size"`
+	Metadata             map[string]interface{} `json:"metadata"`
 }
