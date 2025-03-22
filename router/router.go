f758ec7f9c33f876f3c0552d8e3f2c3d30bce104
dstenkin <danilstenkin57@gmail.com>
2025-03-22 17:21:11 +0500

    feat(router): register PUT /inventory/{id} endpoint

--- router/router.go
+++ router/router.go
@@ -41,6 +41,8 @@ func SetupRouter() {
 	http.HandleFunc("/inventory/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			handlers.GetInventoryByIDHandler(w, r)
+		} else if r.Method == http.MethodPut {
+			handlers.UpdateInventoryHandler(w, r)
 		} else {
 			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		}
